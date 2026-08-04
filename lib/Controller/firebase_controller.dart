ec2ffc6a56e480633d7ee23db3c1d8a61d4294f3
Murtaza <murtazamandasore@gmail.com>
2026-08-04 16:01:26 +0500

    Created firebase_controller

---
+++ lib/Controller/firebase_controller.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:form_manager/Model/form_data_model.dart';
+import 'package:form_manager/Model/person_model.dart';
+
+class FirebaseController {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  CollectionReference get _peopleRef => _firestore.collection('people');
+  CollectionReference get _formsRef => _firestore.collection('forms');
+
+  Stream<List<PersonModel>> getPeopleStream() {
+    return _peopleRef.snapshots().map((snapshot) {
+      return snapshot.docs
+          .map((doc) => PersonModel.fromFirestore(doc))
+          .toList();
+    });
+  }
+
+  Future<void> addPerson(PersonModel person) async {
+    await _peopleRef.doc(person.id).set(person.toMap());
+  }
+
+  Future<void> submitForm(FormDataModel formData) async {
+    final String docid = '${formData.personId}_form_${formData.formNumber}';
+
+    await _formsRef.doc(docid).set(formData.toFirestorMap());
+
+    await _peopleRef.doc(formData.personId).update({
+      'completedFormsCount': FieldValue.increment(1),
+    });
+  }
+
+  Future<FormDataModel?> getSubmittedForm(
+    String personId,
+    int formNumber,
+  ) async {
+    final String docId = '${personId}_form_$formNumber';
+    final doc = await _formsRef.doc(docId).get();
+
+    if (doc.exists) {
+      return FormDataModel.fromFirestore(doc);
+    }
+    return null;
+  }
+}
