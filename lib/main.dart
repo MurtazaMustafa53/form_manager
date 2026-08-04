761ea3d0f3eac58a3f7d32178ec50ba0707838a4
Murtaza <murtazamandasore@gmail.com>
2026-08-04 20:28:35 +0500

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:form_manager/Controller/local_storage_controller.dart';
 import 'package:form_manager/Controller/provider_controller.dart';
-import 'package:form_manager/Views/dashboard_view.dart';
 import 'package:form_manager/Views/login_view.dart';
 import 'package:form_manager/firebase_options.dart';
 import 'package:provider/provider.dart';
